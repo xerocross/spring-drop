5b8cb33a61ec9129e4c5bba036ba889f515e7b94
Adam Cross <adamfgcross@gmail.com>
2021-09-13 10:42:07 -0400

    add initial drop files

---
+++ src/main/java/com/adamfgcross/spring/drop/DropApplication.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.spring.drop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/spring/drop/DropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.spring.drop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
